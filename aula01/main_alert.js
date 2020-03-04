61bba586e70027131592a46445dc08b065e00041
Johni <johnatanosterloh@gmail.com>
2020-03-03 22:06:39 -0300

    Declaração de variáveis, impressão de dados com alert() e console.log(), concatenação entre variáveis, soma e multiplicação entre variáveis, alteração de valores de variáveis com replace(), alteração do estado da variável para maiúscula com toUpperCase() ou minúscula com toLowerCase()

---
+++ aula01/main_alert.js
@@ -0,0 +1,21 @@
+/** EX: 01 - linkando js no html e imprimindo o resultado*/
+// alert("Projeto Javascript - Alert linkado")
+
+/** EX: 02 - declaracao de variaveis e impressao de dados*/
+// var nome = "Johnatan";
+// alert("Bem vindo " + nome);
+
+/** EX: 03 - concatenacao de variaveis com operador + e impressao de dados*/
+// var nome = "Johnatan";
+// var idade = 29;
+// alert(nome + " tem " + idade + " anos");
+
+/** EX: 04 - soma de variaveis, devido as variaveis serem numericas*/
+// var idade = 29;
+// var idade2 = 10;
+// alert(idade + idade2);
+
+/** EX: 05 - concatenacao de variveis, por mais que sejam numericas mas estao entre " tornando string*/
+var idade = "29";
+var idade2 = "10";
+alert(idade + idade2);
---
+++ aula01/main_consoleLog.js
@@ -0,0 +1,25 @@
+/** Ex: 01 - impressao de dados com console log e soma de duas variaveis devido serem numericas e nao string para concatenar */
+// var nome = "Johnatan";
+// var idade = 29;
+// var idade2 = 10;
+// console.log(nome);
+// console.log(idade + idade2);
+
+/** Ex: 02 - alteracao de valores das variaveis durante a execucao */
+// var frase = "Japão é o melhor time do mundo!!!"
+// console.log(frase);
+// console.log(frase.replace("Japão", "Brasil"));
+// alert(frase.replace("Japão", "Real Madrid"));
+
+/** Ex: 03 - imprimir a var toda em maiscula */
+// var nome = "JohnAtaN";
+// console.log(nome.toUpperCase());
+
+/** Ex: 04 - imprimir a var toda em minuscula */
+// var nome = "JohnAtaN";
+// console.log(nome.toLowerCase());
+
+/** Ex: 05 - multiplicacao entre variaveis */
+var n1 = 5;
+var n2 = 2;
+console.log(n1 * n2);
