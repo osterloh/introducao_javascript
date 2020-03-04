2ad480fe403c4641a75ac7de397f1cd762bc308c
Johni <johnatanosterloh@gmail.com>
2020-03-03 23:01:43 -0300

    Declaração de Vetor, retnaando o dado de uma posição específica do vetor, inserindo um novo dado no vetor, removendo o ultimo dado do vetor, verificando a quantia de elementos contidos no vetor, ordenando o vetor do ultimo para o primeiro elemento, alterando o tipo do vetor para string, alterando o separador entre os elementos do vetor e declarando objetos

---
+++ aula02/main_array.js
@@ -0,0 +1,40 @@
+/** Ex: 01 - declaracao de um vetor */
+// var lista = ["maça", "pera", "uva"];
+// console.log(lista);
+
+/** Ex: 02 - impressao de um dado especifico do vetor */
+// var lista = ["maça", "pera", "uva"];
+// console.log(lista[1]);
+
+/** Ex: 03 - adicionar um novo dado no final do vetor */
+// var lista = ["maça", "pera", "uva"];
+// console.log(lista);
+// lista.push("abacate");
+// console.log(lista);
+
+/** Ex: 04 - remover o ultimo dado do vetor */
+// var lista = ["maça", "pera", "uva"];
+// console.log(lista);
+// lista.pop();
+// console.log(lista);
+
+/** Ex: 05 - retornar a quantia de elementos que o vetor possui */
+// var lista = ["maça", "pera", "uva"];
+// console.log(lista.length);
+
+/** Ex: 06 - imprime os dado do vetor do ultimo para o primeiro  */
+// var lista = ["maça", "pera", "uva"];
+// console.log(lista.reverse());
+
+/** Ex: 07 - retorna os dados do vetor em formato de strig e nao como objeto */
+// var lista = ["maça", "pera", "uva"];
+// console.log(lista.toString());
+
+/** Ex: 08 - retornar o primeiro elemento do vetor e o primeiro elemento do toString */
+// var lista = ["maça", "pera", "uva"];
+// console.log(lista[0]);
+// console.log(lista.toString()[0]);
+
+/** Ex: 09 - converte o vetor para toStrig e separa os dados do vetor conforme a opcao desejada, no Ex utilizamos o - */
+var lista = ["maça", "pera", "uva"];
+console.log(lista.join(" - "));
---
+++ aula02/main_objeto.js
@@ -0,0 +1,18 @@
+/** Ex: 01 - definicao de objeto */
+// var pessoa = {
+//     nome: "Johnatan", idade: 29
+// };
+// console.log(pessoa);
+
+/** Ex: 02 - retornando um determinado dado do objeto */
+// var pessoa = {
+//     nome: "Johnatan", idade: 29
+// };
+// console.log(pessoa.idade);
+
+/** Ex: 04 - definicao de objeto com varias posicoes */
+var pessoas = [
+    {nome: "Johnatan", idade: 29},
+    {nome: "Mayara", idade: 25}
+];
+console.log(pessoas[1].nome);
